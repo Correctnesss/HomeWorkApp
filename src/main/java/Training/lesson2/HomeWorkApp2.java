2d31aa49b7db0a8ae449610ca6c51ada781ac0d6
Correctnesss <dunamit@mail.ri>
2024-09-09 20:21:24 +0300

    init

---
+++ src/main/java/Training/lesson2/HomeWorkApp2.java
@@ -0,0 +1,43 @@
+package Training.lesson2;
+
+public class HomeWorkApp2 {
+    public static void main(String[] args) {
+        System.out.println(within10and20(3,10));
+        System.out.println(isPositiveOrNegative(-1000));
+        System.out.println(isNegative(-65));
+        System.out.println(PrintWordNTimes("string",
+                2));
+        System.out.println(leapYear(20));
+    }
+
+
+    public static boolean within10and20(int x1, int x2) {
+        return (10 <= x1 + x2) && (x1 + x2 <= 20);
+    }
+
+    public static String isPositiveOrNegative(int a) {
+        if (a >= 0) {
+            return "Число положительное";
+        }
+        return "Число отрицательное";
+    }
+
+    public static boolean isNegative(int x) {
+        return x<0;
+    }
+
+    public static void PrintWordNTimes(String word, int times) {
+        for (int i = 1; i < times; i++) {
+            System.out.println(word);
+        }
+    }
+
+    public static boolean leapYear(int year) {
+        return year % 4 == 0 & year % 100 != 0||year % 400 == 0;
+    }
+}
+
+
+
+
+
