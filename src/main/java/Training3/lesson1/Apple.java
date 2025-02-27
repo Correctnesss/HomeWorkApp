e03458e98d4e0e5617f13b1b1896ce1bed711f87
Correctnesss <dunamit@mail.ri>
2025-02-27 12:00:05 +0300

    lesson 1

---
+++ src/main/java/Training3/lesson1/Apple.java
@@ -0,0 +1,12 @@
+package Training3.lesson1;
+
+public class Apple extends Fruit {
+    Apple() {
+        super(1.0f);
+    }
+
+    @Override
+    public String toString(){
+        return "яблоки";
+    }
+}
